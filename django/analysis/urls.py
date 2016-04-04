73b1c060f1b0bd020d684a9acb880e57c7e747aa
sbhr <sbhr.123@gmail.com>
2016-04-05 00:11:21 +0900

    make sumple top page

---
+++ django/analysis/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from analysis import views
+
+urlpatterns = [
+    # Top page
+    url(r'$', views.index, name='index'),
+]
--- django/analysis/views.py
+++ django/analysis/views.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponse
+# from analysis.models import Analysis
 
 # Create your views here.
+def index(request):
+    """Top Page"""
+    return render(request,
+                  'analysis/index.html'
+                  )
