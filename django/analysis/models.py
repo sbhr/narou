363619a041e8c99d87b2101206d4284af730834d
sbhr <sbhr.123@gmail.com>
2016-04-04 23:01:53 +0900

    re create django

---
+++ django/analysis/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+class Title(models.Model):
+    # 小説のタイトル
+    name = models.TextField('タイトル')
+
+
+class Pos(models.Model):
+    # 品詞の種類
+    type = models.CharField('種類', max_length = 127, unique = True)
+
+    def __unicode__(self):
+        return self.type
+
+
+class Term(models.Model):
+    # 期間の種類
+    type = models.CharField('期間', max_length = 127, unique = True)
+
+    def __str__(self):
+        return self.type
+
+
+class Letter(models.Model):
+    # 文字
+    pos = models.ForeignKey(Pos, related_name='letters_pos')
+    term = models.ForeignKey(Term, related_name='letters_term')
+    value = models.CharField('文字', max_length = 255)
+    date = models.DateTimeField('収集日時')
+
+    def __unicode__(self):
+        return self.value
+
+
+class Score(models.Model):
+    # 得点や順位
+    title = models.ForeignKey(Title, related_name='scores_title')
+    term = models.ForeignKey(Term, related_name='scores_term')
+    rank = models.IntegerField('順位')
+    point = models.IntegerField('ポイント')
+    date = models.DateTimeField('収集日時')
