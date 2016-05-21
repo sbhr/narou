c08e01a6914c8f2b5adde9c75ad8473ee2ec6c67
sbhr <sbhr.123@gmail.com>
2016-05-22 02:20:59 +0900

    fix code

--- django/analysis/views.py
+++ django/analysis/views.py
@@ -13,9 +13,11 @@ from datetime import datetime
 def index(request):
     """Top page"""
 
-    #本日のランキング
+    # var
     raw_latest_date = Score.objects.order_by('id').reverse()[:1].values('date')[0]['date']
+    # daily total rankig
     words = Letter.objects.filter(pos_id=2, term_id=3, date=raw_latest_date).values('value').annotate(num_words=Count('value')).order_by('-num_words')[:10]
+    # add related title of novels
     for word in words:
         temp_word = word['value']
         word['related_novels'] = Score.objects.filter(term_id=3, date=raw_latest_date, title__name__contains=temp_word).select_related().all().order_by('rank')[:3]
@@ -35,6 +37,7 @@ def about(request):
 def ranking(request, term_name=u"総合"):
     """Ranking Page"""
 
+    #var
     genre_list = [u"総合", u"文学", u"恋愛", u"歴史", u"推理", u"ファンタジー", u"SF", u"ホラー", u"コメディー", u"冒険", u"学園", u"戦記", u"童話", u"詩", u"エッセイ", u"その他"]
     selected_term = term_name
     raw_latest_date = Score.objects.order_by('id').reverse()[:1].values('date')[0]['date']
@@ -43,6 +46,8 @@ def ranking(request, term_name=u"総合"):
     # Form
     if request.method == 'POST':
         form = TermForm(request.POST)
+
+        # Validation
         if form.is_valid():
             date_from = form.cleaned_data['From']
             date_to   = form.cleaned_data['To']
@@ -70,68 +75,80 @@ def ranking(request, term_name=u"総合"):
 class RankingList(ListView):
     """Ranking List"""
 
+    # var
     context_object_name='words'
     template_name='analysis/ranking_list.html'
     paginate_by = 50
 
     def get(self, request, *args, **kwargs):
+        # var
         raw_latest_date = Score.objects.order_by('id').reverse()[:1].values('date')[0]['date']
+        # Get popular words
         words = Letter.objects.filter(pos_id=2, term_id=int(kwargs['term_id']), date=raw_latest_date).values('value').annotate(num_words=Count('value')).order_by('-num_words')
+        # add related title of novels
         for (i, word) in enumerate(words):
                 temp_word = word['value']
                 word['id'] = i + 1
                 word['related_novels'] = Score.objects.filter(term_id=int(kwargs['term_id']), date=raw_latest_date, title__name__contains=temp_word).select_related().all().order_by('rank')[:3]
-        self.object_list = words
 
+        self.object_list = words
         context = self.get_context_data(object_list=self.object_list)
+
         return self.render_to_response(context)
 
 # Search Title
 def search_letter(request):
     """Search letter"""
 
+    # var
     template_name='analysis/search_letter.html'
-    paginate_by = 10
     target_terms = Term.objects.all()
     form = SearchLetterForm()
 
+    # Form
     if request.method == 'POST':
         form = SearchLetterForm(request.POST)
+        # Validation
         if form.is_valid():
+            # var for search
             _word = form.cleaned_data['word']
             _term = request.POST['term']
-
-            words = Letter.objects.filter(term_id=_term, value__contains=_word).values('value').annotate(num_words=Count('value')).order_by('-num_words')
-
+            # Get Letters contain var for search
+            letters = Letter.objects.filter(term_id=_term, value__contains=_word).values('value').annotate(num_letters=Count('value')).order_by('-num_letters')
     else:
         form = SearchLetterForm()
-        words = ''
+        letters = ''
 
     return render(request,
                   'analysis/search_letter.html',
                   {'form':form,
                    'target_terms':target_terms,
-                   'words':words})
+                   'letters':letters})
 
 # Search Title
 def search_title(request):
     """Search title"""
 
+    # var
     template_name='analysis/search_title.html'
     paginate_by = 50
     form = SearchTitleForm
     query = request.GET.get('query')
 
+    # Form
     if request.method == 'POST' or query is not None:
+        # POST or Paging
         if query is None:
             form = SearchTitleForm(request.POST)
         else:
             form = SearchTitleForm({'word':query})
         if form.is_valid() or query is not None:
+            # var for search
             _word = form.cleaned_data['word']
+            # Get titles contain var for search
+            title_list = Title.objects.filter(name__contains=_word).values('name')
 
-            title_list = Title.objects.filter(name__contains=_word).all().values('name')
-
+            # Paging
             paginator = Paginator(title_list, paginate_by)
             page = request.GET.get('page')
             try:
