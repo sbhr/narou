1cf37673689837ab986777749798bff86f9f083d
sbhr <sbhr.123@gmail.com>
2016-05-22 21:32:45 +0900

    update top page. show overview of data

--- django/analysis/views.py
+++ django/analysis/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.db.models import Count
 from django.views.generic.list import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from analysis.models import Score, Letter, Term, Title
+from analysis.models import Score, Letter, Term, Title, Overview
 from analysis.forms import TermForm, SearchLetterForm, SearchTitleForm
 from datetime import datetime
 
@@ -15,6 +15,7 @@ def index(request):
 
     # var
     raw_latest_date = Score.objects.order_by('id').reverse()[:1].values('date')[0]['date']
+
     # daily total rankig
     words = Letter.objects.filter(pos_id=2, term_id=3, date=raw_latest_date).values('value').annotate(num_words=Count('value')).order_by('-num_words')[:10]
     # add related title of novels
@@ -22,9 +23,13 @@ def index(request):
         temp_word = word['value']
         word['related_novels'] = Score.objects.filter(term_id=3, date=raw_latest_date, title__name__contains=temp_word).select_related().all().order_by('rank')[:3]
 
+    # Overview of Data
+    overview = Overview.objects.all().order_by('-id')[:1]
+
     return render(request,
                   'analysis/index.html',
-                  {'words': words})
+                  {'words': words,
+                   'overview':overview})
 
 # About page
 def about(request):
