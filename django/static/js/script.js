3dba2302aa8850324409c29170ccd304f845f7fc
sbhr <sbhr.123@gmail.com>
2016-05-27 21:39:27 +0900

    delete alert.

--- django/static/js/script.js
+++ django/static/js/script.js
@@ -2,6 +2,5 @@
 		$(".dropdown-menu li a").click(function(){
 			$(this).parents('.dropdown').find('.dropdown-toggle').html($(this).text() + ' <span class="caret"></span>');
 			$(this).parents('.dropdown').find('input[name="dropdown-value"]').val($(this).attr("data-value"));
-			alert($(this).text());
 		});
 	});
